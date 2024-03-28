d12aa3f9e742a757ee739a8ef608278aa986c50b
(20007604) Maurice Brazil <20007604@wit.ie>
2024-03-28 15:34:12 +0000

    Fix upcoming movies page.

--- movies/src/pages/upcomingPage.js
+++ movies/src/pages/upcomingPage.js
@@ -3,6 +3,7 @@ import PageTemplate from '../components/templateMovieListPage'
 import { getUpcomingMovies } from "../api/tmdb-api";
 import { useQuery } from 'react-query';
 import Spinner from '../components/spinner';
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
 
 const UpcomingMoviesPage = (props) => {
 
@@ -26,7 +27,9 @@ const UpcomingMoviesPage = (props) => {
     <PageTemplate
       title='Discover Upcoming Movies'
       movies={movies}
-      selectFavorite={addToFavorites}
+      action={(movie) => {
+        return <AddToFavoritesIcon movie={movie} />
+      }}
     />
   );
 };
