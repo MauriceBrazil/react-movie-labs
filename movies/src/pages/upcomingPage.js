06bc9efdf2f80b834098c32e5a9930c71c9b446d
(20007604) Maurice Brazil <20007604@wit.ie>
2024-03-28 15:39:42 +0000

    New button for upcoming movies page.

--- movies/src/pages/upcomingPage.js
+++ movies/src/pages/upcomingPage.js
@@ -3,7 +3,7 @@ import PageTemplate from '../components/templateMovieListPage'
 import { getUpcomingMovies } from "../api/tmdb-api";
 import { useQuery } from 'react-query';
 import Spinner from '../components/spinner';
-import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+import AddToPlaylistIcon from '../components/cardIcons/addToPlaylist'
 
 const UpcomingMoviesPage = (props) => {
 
@@ -28,7 +28,7 @@ const UpcomingMoviesPage = (props) => {
       title='Discover Upcoming Movies'
       movies={movies}
       action={(movie) => {
-        return <AddToFavoritesIcon movie={movie} />
+        return <AddToPlaylistIcon movie={movie} />
       }}
     />
   );
